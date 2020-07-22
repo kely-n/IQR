343462322407c1e335c38fadd90916913c54e264
kely-n <kelyukiru@gmail.com>
2020-07-22 12:05:51 +0300

    final thing

--- src/iqr/interQuartileRange.java
+++ src/iqr/interQuartileRange.java
@@ -63,10 +63,12 @@ public class interQuartileRange {
            sum+=f;
            if(sum<=midpoint){
                firstHalf.put(element, mainHash.get(element));
-           }else if(sum>midpoint && current_sum<midpoint){
+           }else if(sum>midpoint && current_sum<=midpoint){
                //find amount left to reach midpoint, and add it with value to firstHalf
                int amount_left_to_reach_Midpoint =  midpoint - current_sum;
-               firstHalf.put(element,amount_left_to_reach_Midpoint);
+               if(amount_left_to_reach_Midpoint>0) {
+                   firstHalf.put(element, amount_left_to_reach_Midpoint);
+               }
                //now find amount left from current frequency, which is either
                //current frequency f - amount_left_to_reach_Midpoint or
                //sum - midpoint
@@ -81,14 +83,19 @@ public class interQuartileRange {
                    //current_sum+=1;
                }
                //if amount_left_from_current_frequency is not 0 add it to the secondHalf.
-               if(amount_left_from_current_frequency!=0){
+               if(amount_left_from_current_frequency>0){
                    secondHalf.put(element,amount_left_from_current_frequency);
                }
 
            }else {
                if(isodd && medianHasNotBeenRemoved){
-                   int amount_after_removing_median = (int)mainHash.get(element) - 1;
-                   secondHalf.put(element,amount_after_removing_median);
+                   System.out.println(element);
+                   if(mainHash.containsKey(element)) {
+                       int amount_after_removing_median = (int) mainHash.get(element) - 1;
+                       if (amount_after_removing_median > 0) {
+                           secondHalf.put(element, amount_after_removing_median);
+                       }
+                   }
                    medianHasNotBeenRemoved = false;
                }else {
                    secondHalf.put(element,mainHash.get(element));
@@ -128,11 +135,14 @@ public class interQuartileRange {
             if(sum<=midpoint){
                 firstFirstHalf.put(element, firstHalf.get(element));
                 first_num = element;
-            }else if(sum>midpoint && current_sum<midpoint){
+            }else if(sum>midpoint && current_sum<=midpoint){
                 //find amount left to reach midpoint, and add it with value to firstHalf
                 int amount_left_to_reach_Midpoint =  midpoint - current_sum;
-                firstFirstHalf.put(element,amount_left_to_reach_Midpoint);
-                first_num = element;
+                if(amount_left_to_reach_Midpoint>0) {
+                    firstFirstHalf.put(element, amount_left_to_reach_Midpoint);
+                    first_num = element;
+                }
+
                 //set current_sum to midpoint or add amount_left_to_reach_Midpoint to the current sum
                 current_sum += amount_left_to_reach_Midpoint;
                 //now find amount left from current frequency, which is either
@@ -145,23 +155,27 @@ public class interQuartileRange {
                     //remove the median, which wont appear in the firstHalf or secondHalf
                     amount_left_from_current_frequency-=1;
                     medianHasNotBeenRemoved = false;
-                    System.out.println("ehid" +element);
                     return element;
                     //update current sum
                     //current_sum+=1;
                 }
                 //if amount_left_from_current_frequency is not 0 add it to the secondHalf.
-                if(amount_left_from_current_frequency!=0){
+                if(amount_left_from_current_frequency>0){
                     secondFirstHalf.put(element,amount_left_from_current_frequency);
                     second_num = element;
                 }
 
             }else {
                 if(isodd && medianHasNotBeenRemoved){
-                    int amount_after_removing_median = (int)firstHalf.get(element) - 1;
-                    secondFirstHalf.put(element,amount_after_removing_median);
+                    if(firstHalf.containsKey(element)) {
+                        int amount_after_removing_median = (int) firstHalf.get(element) - 1;
+                        if (amount_after_removing_median > 0) {
+                            secondFirstHalf.put(element, amount_after_removing_median);
+                        }
+                        second_num = element;
+                    }
                     medianHasNotBeenRemoved = false;
-                    second_num = element;
+
                 }else {
                     //if second num has not been set, grab it first
                     if(second_num==0){
@@ -174,7 +188,10 @@ public class interQuartileRange {
             current_sum = sum;
         }
         //return the median
-        return (first_num+second_num)/2;
+        //System.out.println("first: "+first_num+" second: "+second_num);
+        double a = (double) first_num;
+        double b = (double) second_num;
+        return (a+b)/2;
     }
 
     private double divideSecondHalf(int[] elements, int totalFrequency){
@@ -206,11 +223,13 @@ public class interQuartileRange {
             if(sum<=midpoint){
                 firsSecondtHalf.put(element, secondHalf.get(element));
                 first_num = element;
-            }else if(sum>midpoint && current_sum<midpoint){
+            }else if(sum>midpoint && current_sum<=midpoint){
                 //find amount left to reach midpoint, and add it with value to firstHalf
                 int amount_left_to_reach_Midpoint =  midpoint - current_sum;
-                firsSecondtHalf.put(element,amount_left_to_reach_Midpoint);
-                first_num = element;
+                if(amount_left_to_reach_Midpoint>0) {
+                    firsSecondtHalf.put(element, amount_left_to_reach_Midpoint);
+                    first_num = element;
+                }
                 //set current_sum to midpoint or add amount_left_to_reach_Midpoint to the current sum
                 current_sum += amount_left_to_reach_Midpoint;
                 //now find amount left from current frequency, which is either
@@ -223,7 +242,6 @@ public class interQuartileRange {
                     //remove the median, which wont appear in the firstHalf or secondHalf
                     amount_left_from_current_frequency-=1;
                     medianHasNotBeenRemoved = false;
-                    System.out.println("ehid" +element);
                     return element;
                     //update current sum
                     //current_sum+=1;
@@ -236,10 +254,14 @@ public class interQuartileRange {
 
             }else {
                 if(isodd && medianHasNotBeenRemoved){
-                    int amount_after_removing_median = (int)secondHalf.get(element) - 1;
-                    secondSecondHalf.put(element,amount_after_removing_median);
+                    if(secondHalf.containsKey(element)) {
+                        int amount_after_removing_median = (int) secondHalf.get(element) - 1;
+                        if (amount_after_removing_median > 0) {
+                            secondSecondHalf.put(element, amount_after_removing_median);
+                        }
+                        second_num = element;
+                    }
                     medianHasNotBeenRemoved = false;
-                    second_num = element;
                 }else {
                     //if second num has not been set, grab it first
                     if(second_num==0){
@@ -252,7 +274,10 @@ public class interQuartileRange {
             current_sum = sum;
         }
         //return the median
-        return (first_num+second_num)/2;
+        //System.out.println("first: "+first_num+" second: "+second_num);
+        double a = (double) first_num;
+        double b = (double) second_num;
+        return (a+b)/2;
     }
     public static void main(String ... args){
         Iterator it;
@@ -291,17 +316,20 @@ public class interQuartileRange {
 
             //confirming if first division was right
             //print values.
-//            it = interQuartileRange.firstHalf.entrySet().iterator();
-//            while (it.hasNext()){
-//                Map.Entry obj  = (Map.Entry)it.next();
-//                System.out.println(obj.getValue());
-//            }
-//            System.out.println("second half");
-//            it = interQuartileRange.secondHalf.entrySet().iterator();
-//            while (it.hasNext()){
-//                Map.Entry obj  = (Map.Entry)it.next();
-//                System.out.println(obj.getValue());
-//            }
+    /*
+            it = interQuartileRange.firstHalf.entrySet().iterator();
+            while (it.hasNext()){
+                Map.Entry obj  = (Map.Entry)it.next();
+                System.out.println(obj.getKey()+" : "+obj.getValue());
+            }
+            System.out.println("second half");
+            it = interQuartileRange.secondHalf.entrySet().iterator();
+            while (it.hasNext()){
+                Map.Entry obj  = (Map.Entry)it.next();
+                System.out.println(obj.getKey()+" : "+obj.getValue());
+            }
+
+     */
 
             //get elements and frequency values from firstHalf and secondHalf.
             int[] firstHalf_elements = new int[interQuartileRange.firstHalf.size()];
@@ -320,10 +348,10 @@ public class interQuartileRange {
             //sort array and divide it further into 2
             firstHalf_elements = interQuartileRange.sortArray(firstHalf_elements);
             double lowerQuartile = interQuartileRange.dividefirstHalf(firstHalf_elements, summation_frequency_firstHalf);
-            System.out.println("first quartile: "+lowerQuartile);
+            System.out.println("lower quartile: "+lowerQuartile);
             //print it
             //confirming if division was right
-
+/*
             System.out.println("firstFirstHalf");
             it = interQuartileRange.firstFirstHalf.entrySet().iterator();
             while (it.hasNext()){
@@ -338,9 +366,11 @@ public class interQuartileRange {
             }
 
 
+ */
+
 
             //second half
-            int[] secondHalf_elements = new int[interQuartileRange.firstHalf.size()];
+            int[] secondHalf_elements = new int[interQuartileRange.secondHalf.size()];
             int summation_frequency_secondHalf = 0;
             //firstHalf
             it = interQuartileRange.secondHalf.entrySet().iterator();
@@ -355,10 +385,10 @@ public class interQuartileRange {
             //check if
             secondHalf_elements = interQuartileRange.sortArray(secondHalf_elements);
             double upperQuartile = interQuartileRange.divideSecondHalf(secondHalf_elements, summation_frequency_secondHalf);
-            System.out.println("second quartile: "+ upperQuartile);
+            System.out.println("upper quartile: "+ upperQuartile);
 
             //confirming if division is right
-
+/*
             System.out.println(" first second half");
             it = interQuartileRange.firsSecondtHalf.entrySet().iterator();
             while (it.hasNext()){
@@ -376,6 +406,8 @@ public class interQuartileRange {
             }
 
 
+
+ */
             System.out.println("interQuartile range: " +(upperQuartile - lowerQuartile));
         }
     }
